7a7a424614decfbbfd69023747f0bfa92e97f234
Ilya Kurnosenkov <ilya.kurnosenkov@arcadia.spb.ru>
2018-05-03 19:59:37 +0300

    Trigger Navroid animation with delays (WIP).

--- src/app/pages/near-litmo/near-litmo.page.ts
+++ src/app/pages/near-litmo/near-litmo.page.ts
@@ -1,16 +1,40 @@
-import { Component } from '@angular/core';
-import { Scene } from '../scene';
-import { InventoryService } from '../../services/inventory.service';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {Scene} from '../scene';
+import {InventoryService} from '../../services/inventory.service';
+import {setTimeout} from 'timers';
 
 @Component({
   templateUrl: './near-litmo.page.html',
   styleUrls: ['./near-litmo.css']
 })
-export class NearLitmoPage extends Scene {
+export class NearLitmoPage extends Scene implements OnInit {
+
+  @ViewChild('navroid') navroid: any;
 
   constructor(
     inv: InventoryService
   ) {
     super();
+
+  }
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.startNavroidAnimation();
+    }, 1000);
+  }
+
+  private startNavroidAnimation() {
+    const el = this.navroid.nativeElement;
+    el.style.animationPlayState = 'running';
+
+    // restart Navroid animation each 6s
+    setInterval(() => {
+      console.log('restarting animation');
+      el.style.animation = '';
+      setTimeout(() => {
+        el.style.animation = 'near-litmo-navroid-animation';
+      }, 0);
+    }, 3000);
   }
 }
