0887e4fb3990b5a20ca80505f2db82d27db5758d
Ilya Kurnosenkov <ilya.kurnosenkov@arcadia.spb.ru>
2018-05-03 18:44:15 +0300

    Added background to intro, and cleaned up intro animation.

--- src/app/pages/start/start.page.ts
+++ src/app/pages/start/start.page.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
-  templateUrl: './start.page.html'
+  templateUrl: './start.page.html',
+  styleUrls: ['./start.css']
 })
 export class StartPage {
 
